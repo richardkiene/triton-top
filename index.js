f35201b6b89ba64f7dddba54c1e0dbc45e67f821
Richard Kiene <richard.kiene@joyent.com>
2017-03-30 17:53:06 +0000

    Initial foundation for calculation

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var PORT = ':9163';
 var METRICS = '/metrics';
 var DISCO = '/v1/discover';
 var FETCH_INTERVAL_MS = 10 * 1000; /* 1000ms per second */
+var REFRESH_INTERVAL_MS = 300 * 1000; /* 1000ms per second */
 
 var options = [
     {
@@ -42,7 +43,9 @@ var options = [
     }
 ];
 
+var opts, json_client;
 var parser = mod_dashdash.createParser({ options: options});
+var targets = {};
 
 function _showHelp () {
     var help = parser.help({ includeEnv: true }).trimRight();
@@ -51,52 +54,95 @@ function _showHelp () {
 }
 
 try {
-    var opts = parser.parse(process.argv);
+    opts = parser.parse(process.argv);
 } catch (e) {
     console.error('error: %s', e.message);
     process.exit(1);
 }
 
 if (opts.help) {
-    console.log(opts);
     _showHelp();
 } else if (opts.endpoint && opts.cert && opts.key) {
-    var json_client = mod_restify.createJsonClient({
-        url: HTTPS + opts.endpoint + PORT,
-        rejectUnauthorized: false,
-        cert: mod_fs.readFileSync(opts.cert),
-        key: mod_fs.readFileSync(opts.key)
-    });
+    mod_vasync.pipeline({
+        'funcs': [
+            function createClients(arg, next) {
+                json_client = mod_restify.createJsonClient({
+                    url: HTTPS + opts.endpoint + PORT,
+                    rejectUnauthorized: false,
+                    cert: mod_fs.readFileSync(opts.cert),
+                    key: mod_fs.readFileSync(opts.key)
+                });
 
-    var str_client = mod_restify.createStringClient({
-        url: HTTPS + opts.endpoint + PORT,
-        rejectUnauthorized: false,
-        cert: mod_fs.readFileSync(opts.cert),
-        key: mod_fs.readFileSync(opts.key)
-    });
 
-    json_client.get(DISCO, function(err, req, res, obj) {
-        mod_assert.ifError(err);
-        var target_array = obj.containers;
-        var targets = {};
-        for (var i = 0; i < target_array.length; i++) {
-            var target = target_array[i];
-            var uuid = target.vm_uuid;
-            var endpoint = opts.endpoint;
-            var url = HTTPS + uuid + '.' + endpoint + PORT + METRICS;
-            target.cmon_url = url;
-            targets[uuid] = target;
-        }
-
-        var target_keys = Object.keys(targets);
-
-        setInterval(function () {
-            for (var j = 0; j < target_keys.length; j++) {
-                var fetch_target = targets[target_keys[j]];
-
+                next();
+            },
+            function initialRefresh(arg, next) {
+                refreshTargets(next);
+            },
+            function initialMetrics(arg, next) {
+                fetchAllMetrics(next);
             }
-        }, FETCH_INTERVAL_MS);
+        ]
+    }, function _p(p_err, result) {
+        mod_assert.ifError(p_err);
     });
 } else {
     _showHelp();
+}
+
+function refreshTargets(cb) {
+    json_client.get(DISCO, function(err, req, res, obj) {
+        mod_assert.ifError(err);
+        var target_array = obj.containers;
+
+        mod_vasync.forEachPipeline({
+            'inputs': target_array,
+            'func': function (target, next) {
+                targets[target.vm_uuid] = target;
+                next();
+            }
+        }, function (err, result) {
+            mod_assert.ifError(err);
+            cb();
+        });
+    });
+}
+
+function fetchAllMetrics(cb) {
+    mod_vasync.forEachPipeline({
+        'inputs': Object.keys(targets),
+        'func': function fetchMetrics(key, next) {
+            var str_client = mod_restify.createStringClient({
+                url: HTTPS + targets[key].vm_uuid + '.' + opts.endpoint + PORT,
+                rejectUnauthorized: false,
+                cert: mod_fs.readFileSync(opts.cert),
+                key: mod_fs.readFileSync(opts.key)
+            });
+
+            str_client.get(METRICS, function(err, req, res, data) {
+                mod_assert.ifError(err);
+                chunked = data.trim().split('\n');
+                var i = 0;
+                while(i < chunked.length) {
+                    var help = chunked[i++];
+                    var type = chunked[i++];
+                    var valstr = chunked[i++].split(' ');
+                    var name = valstr[0];
+                    var value = valstr[1];
+
+                    targets[key].metrics = {
+                        help: help,
+                        type: type,
+                        name: name,
+                        value: value
+                    };
+                }
+
+                next();
+            });
+        }
+    }, function (err, result) {
+        mod_assert.ifError(err);
+        cb();
+    });
 }
