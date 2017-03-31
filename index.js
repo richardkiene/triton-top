f45a432103b292afa7023d95ca15c77e86608124
Richard Kiene <richard.kiene@joyent.com>
2017-03-31 01:03:23 +0000

    Sooooo much hype!

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ var options = [
     }
 ];
 
-var opts, json_client;
+var draw_timeout, opts, json_client;
 var parser = mod_dashdash.createParser({ options: options});
 var targets = {};
 
@@ -83,99 +83,7 @@ if (opts.help) {
                 fetchAllMetrics(next);
             },
             function showStuff(arg, next) {
-                var zkeys = Object.keys(targets);
-                var gauge = mod_clui.Gauge;
-                var line = mod_clui.Line;
-
-                var mem_used = 0;
-                var mem_limit = 0;
-                var swap_used = 0;
-                var swap_limit = 0;
-                var zfs_used = 0;
-                var zfs_avail = 0;
-
-                for (var z = 0; z < zkeys.length; z++) {
-                    var key = zkeys[z];
-                    var cur_metrics = targets[key].cur_metrics;
-
-                    /* Total Memory Calculation */
-                    var mu_inst = cur_metrics['mem_agg_usage'].value;
-                    var ml_inst = cur_metrics['mem_limit'].value;
-                    mem_used += (((mu_inst / 1000) / 1000) / 1000);
-                    mem_limit += (((ml_inst / 1000) / 1000) / 1000);
-
-                    /* Total Swap Calculation */
-                    var su_inst = cur_metrics['mem_swap'].value;
-                    var sl_inst = cur_metrics['mem_swap_limit'].value;
-                    swap_used += (((su_inst / 1000) / 1000) / 1000);
-                    swap_limit += (((sl_inst / 1000) / 1000) / 1000);
-                    
-                    /* Total Swap Calculation */
-                    var zu_inst = cur_metrics['zfs_used'].value;
-                    var za_inst = cur_metrics['zfs_available'].value;
-                    zfs_used += (((zu_inst / 1000) / 1000) / 1000);
-                    zfs_avail += (((za_inst / 1000) / 1000) / 1000);
-
-                    //var alias = targets[key].vm_alias;
-                }
-
-                var GB = ' GB';
-
-                /* Memory Totals Output */
-                var mem_danger = mem_limit * 0.8;
-                var mem_lim_human = mem_limit.toFixed(2) + GB
-                var mem_human = mem_used.toFixed(2) + ' / ' + mem_lim_human;
-                var mem_gauge = gauge(
-                    mem_used,
-                    mem_limit,
-                    20,
-                    mem_danger,
-                    mem_human);
-
-                var mem_line = new line();
-                mem_line.padding(2);
-                mem_line.column('Total Memory Use', 20, [mod_clc.cyan]);
-                mem_line.column(mem_gauge);
-                mem_line.fill();
-                mem_line.output();
-
-                /* Swap Totals Output */
-                var swap_danger = swap_limit * 0.8;
-                var swap_lim_human = swap_limit.toFixed(2) + GB;
-                var swap_human = swap_used.toFixed(2) + ' / ' + swap_lim_human;
-                var swap_gauge = gauge(
-                    swap_used,
-                    swap_limit,
-                    20,
-                    swap_danger,
-                    swap_human);
-
-                var swap_line = new line();
-                swap_line.padding(2);
-                swap_line.column('Total Swap Use', 20, [mod_clc.cyan]);
-                swap_line.column(swap_gauge);
-                swap_line.fill();
-                swap_line.output();
-
-                /* Swap Totals Output */
-                var zfs_limit = (zfs_used + zfs_avail).toFixed(2);
-                var zfs_lim_human = zfs_limit + GB;
-                var zfs_danger = zfs_limit * 0.8;
-                var zfs_human = zfs_used.toFixed(2) + ' / ' + zfs_lim_human;
-                var zfs_gauge = gauge(
-                    zfs_used,
-                    zfs_limit,
-                    20,
-                    zfs_danger,
-                    zfs_human);
-
-                var zfs_line = new line();
-                zfs_line.padding(2);
-                zfs_line.column('Total ZFS use', 20, [mod_clc.cyan]);
-                zfs_line.column(zfs_gauge);
-                zfs_line.fill();
-                zfs_line.output();
-
+                draw();
                 next();
             }
         ]
@@ -184,6 +92,171 @@ if (opts.help) {
     });
 } else {
     _showHelp();
+}
+
+function draw() {
+    console.log(mod_clc.reset);
+
+    var zkeys = Object.keys(targets);
+    var gauge = mod_clui.Gauge;
+    var line = mod_clui.Line;
+
+    var mem_used = 0;
+    var mem_limit = 0;
+    var swap_used = 0;
+    var swap_limit = 0;
+    var zfs_used = 0;
+    var zfs_avail = 0;
+
+    var zone_stats = [];
+
+    for (var z = 0; z < zkeys.length; z++) {
+        var key = zkeys[z];
+        var cur_metrics = targets[key].cur_metrics;
+        var last_metrics = targets[key].last_metrics;
+
+        var stats = {
+            alias: targets[key].vm_alias,
+            mem_use: cur_metrics['mem_agg_usage'].value,
+            mem_lim: cur_metrics['mem_limit'].value,
+            swp_use: cur_metrics['mem_swap'].value,
+            swp_lim: cur_metrics['mem_swap_limit'].value,
+            zfs_use: cur_metrics['zfs_used'].value,
+            zfs_av: cur_metrics['zfs_available'].value
+        };
+
+        zone_stats.push(stats);
+
+        /* Total Memory Calculation */
+        var mu_inst = stats.mem_use;
+        var ml_inst = stats.mem_lim;
+        mem_used += (((mu_inst / 1000) / 1000) / 1000);
+        mem_limit += (((ml_inst / 1000) / 1000) / 1000);
+
+        /* Total Swap Calculation */
+        var su_inst = stats.swp_use;
+        var sl_inst = stats.swp_lim;
+        swap_used += (((su_inst / 1000) / 1000) / 1000);
+        swap_limit += (((sl_inst / 1000) / 1000) / 1000);
+        
+        /* Total Swap Calculation */
+        var zu_inst = stats.zfs_use;
+        var za_inst = stats.zfs_av;
+        zfs_used += (((zu_inst / 1000) / 1000) / 1000);
+        zfs_avail += (((za_inst / 1000) / 1000) / 1000);
+    }
+
+    var GB = ' GB';
+    var blank_line = new line().fill().output();
+
+    /* Memory Totals Output */
+    var mem_danger = mem_limit * 0.8;
+    var mem_lim_human = mem_limit.toFixed(2) + GB
+    var mem_human = mem_used.toFixed(2) + ' / ' + mem_lim_human;
+    var mem_gauge = gauge(
+        mem_used,
+        mem_limit,
+        20,
+        mem_danger,
+        mem_human);
+
+    var mem_line = new line();
+    mem_line.padding(2);
+    mem_line.column('Total Memory Use', 20, [mod_clc.cyan]);
+    mem_line.column(mem_gauge);
+    mem_line.fill();
+    mem_line.output();
+
+    /* Swap Totals Output */
+    var swap_danger = swap_limit * 0.8;
+    var swap_lim_human = swap_limit.toFixed(2) + GB;
+    var swap_human = swap_used.toFixed(2) + ' / ' + swap_lim_human;
+    var swap_gauge = gauge(
+        swap_used,
+        swap_limit,
+        20,
+        swap_danger,
+        swap_human);
+
+    var swap_line = new line();
+    swap_line.padding(2);
+    swap_line.column('Total Swap Use', 20, [mod_clc.cyan]);
+    swap_line.column(swap_gauge);
+    swap_line.fill();
+    swap_line.output();
+
+    /* Swap Totals Output */
+    var zfs_limit = (zfs_used + zfs_avail).toFixed(2);
+    var zfs_lim_human = zfs_limit + GB;
+    var zfs_danger = zfs_limit * 0.8;
+    var zfs_human = zfs_used.toFixed(2) + ' / ' + zfs_lim_human;
+    var zfs_gauge = gauge(
+        zfs_used,
+        zfs_limit,
+        20,
+        zfs_danger,
+        zfs_human);
+
+    var zfs_line = new line();
+    zfs_line.padding(2);
+    zfs_line.column('Total ZFS use', 20, [mod_clc.cyan]);
+    zfs_line.column(zfs_gauge);
+    zfs_line.fill();
+    zfs_line.output();
+
+    blank_line.output();
+
+    var zone_col_names = new line();
+    zone_col_names.padding(2);
+    zone_col_names.column('Name', 20, [mod_clc.cyan]);
+    zone_col_names.column('Memory in MB', 20, [mod_clc.cyan]);
+    zone_col_names.column('Swap in MB', 20, [mod_clc.cyan]);
+    zone_col_names.column('ZFS in GB', 20, [mod_clc.cyan]);
+    zone_col_names.fill();
+    zone_col_names.output();
+
+    for (var l = 0; l < zone_stats.length; l++) {
+        var stats = zone_stats[l];
+        var zone_line = new line();
+        zone_line.padding(2);
+        zone_line.column(stats.alias, 20, [mod_clc.white]);
+
+        /* Mem usage */
+        var mem_use = stats.mem_use;
+        var mem_use_human = (((mem_use / 1000) / 1000));
+        var mem_use_human = mem_use_human.toFixed(1);
+        var mem_lim = stats.mem_lim;
+        var mem_lim_human = (((mem_lim / 1000) / 1000));
+        var mem_lim_human = mem_lim_human.toFixed(1);
+        var mem_col = mem_use_human + ' / ' + mem_lim_human;
+        zone_line.column(mem_col, 20, [mod_clc.white]);
+
+        /* Swap usage */
+        var swp_use = stats.swp_use;
+        var swp_use_human = (((swp_use / 1000) / 1000));
+        var swp_use_human = swp_use_human.toFixed(1);
+        var swp_lim = stats.swp_lim;
+        var swp_lim_human = (((swp_lim / 1000) / 1000));
+        var swp_lim_human = swp_lim_human.toFixed(1);
+        var swp_col = swp_use_human + ' / ' + swp_lim_human;
+        zone_line.column(swp_col, 20, [mod_clc.white]);
+
+        /* ZFS usage */
+        var zfs_use = stats.zfs_use;
+        var zfs_use_human = ((zfs_use / 1000) / 1000) / 1000;
+        var zfs_use_human = zfs_use_human.toFixed(1);
+        var zfs_lim = parseInt(stats.zfs_av) + parseInt(zfs_use);
+        //console.error('zfs_av: ', stats.zfs_av);
+        //console.error('zfs_lim: ', zfs_lim);
+        var zfs_lim_human = ((zfs_lim / 1000) / 1000) / 1000;
+        var zfs_lim_human = zfs_lim_human.toFixed(1);
+        var zfs_col = zfs_use_human + ' / ' + zfs_lim_human;
+        zone_line.column(zfs_col, 20, [mod_clc.white]);
+        zone_line.fill();
+        zone_line.output();
+    }
+
+    draw_timeout = setTimeout(draw, 1000);
 }
 
 function refreshTargets(cb) {
