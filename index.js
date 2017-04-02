78b9847b88a6aaeff8aefc5f3f2cb12d03a44c4c
Richard Kiene <richard.kiene@joyent.com>
2017-04-02 18:16:51 +0000

    So fresh and so clean

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ var mod_dashdash = require('dashdash');
 var mod_fs = require('fs');
 var mod_jsprim = require('jsprim');
 var mod_restify = require('restify-clients');
+var mod_extsprintf = require('extsprintf');
 var mod_vasync = require('vasync');
 
+var GB = 'GB';
+var MB = 'MB';
+var MBps = 'MBps';
 var HTTPS = 'https://';
 var PORT = ':9163';
+var PCT = '%';
 var METRICS = '/metrics';
 var DISCO = '/v1/discover';
 var FETCH_INTERVAL_MS = 10 * 1000; /* 1000ms per second */
 var REFRESH_INTERVAL_MS = 300 * 1000; /* 1000ms per second */
+var NANO_SEC_PERIOD = 10000000000; /* Nano seconds in 10 sec peroid */
 
 var options = [
     {
@@ -75,6 +81,20 @@ if (opts.help) {
     _showHelp();
 }
 
+function bytesToMB(bytes_val, precision) {
+    mod_assert.number(bytes_val, 'bytes_val');
+    mod_assert.number(precision, 'precision');
+
+    return (bytes_val / 1000) / 1000).toFixed(precision);
+}
+
+function bytesToGB(bytes_val, precision) {
+    mod_assert.number(bytes_val, 'bytes_val');
+    mod_assert.number(precision, 'precision');
+
+    return (((bytes_val / 1000) / 1000) / 1000).toFixed(precision);
+}
+
 function paint() {
     mod_vasync.pipeline({
         'funcs': [
@@ -110,13 +130,10 @@ function draw() {
     console.log(mod_clc.reset);
 
     var zkeys = Object.keys(targets);
-    /*var gauge = mod_clui.Gauge;
-    var line = mod_clui.Line;
-    var spark = mod_clui.SparkLine;*/
 
-    var line          = mod_clui.Line,
-        gauge         = mod_clui.Gauge;
-        spark         = mod_clui.Sparkline;
+    var line = mod_clui.Line;
+    var gauge = mod_clui.Gauge;
+    var spark = mod_clui.Sparkline;
 
     var mem_used = 0;
     var mem_limit = 0;
@@ -125,6 +142,7 @@ function draw() {
     var zfs_used = 0;
     var zfs_avail = 0;
     var net_in_mb_per_sec = 0;
+    var net_out_mb_per_sec = 0;
     var cpu_pct = 0;
 
     var zone_stats = [];
@@ -136,23 +154,25 @@ function draw() {
 
         var stats = {
             alias: targets[key].vm_alias,
-            mem_use: cur_metrics['mem_agg_usage'].value,
-            mem_lim: cur_metrics['mem_limit'].value,
-            swp_use: cur_metrics['mem_swap'].value,
-            swp_lim: cur_metrics['mem_swap_limit'].value,
-            zfs_use: cur_metrics['zfs_used'].value,
-            zfs_av: cur_metrics['zfs_available'].value,
-            netb_in: cur_metrics['net_agg_bytes_in'].value,
-            netb_in_old: last_metrics['net_agg_bytes_in'] ?
-                last_metrics['net_agg_bytes_in'].value : 0,
-            netb_out: cur_metrics['net_agg_bytes_out'].value,
-            cpu_usr: cur_metrics['cpu_user_usage'].value,
-            cpu_usr_old: last_metrics['cpu_user_usage'] ?
-                last_metrics['cpu_user_usage'].value : 0,
-            cpu_sys: cur_metrics['cpu_sys_usage'].value,
-            cpu_sys_old: last_metrics['cpu_sys_usage'] ?
-                last_metrics['cpu_sys_usage'].value : 0,
-            load_avg: cur_metrics['load_average'].value
+            mem_use: cur_metrics.mem_agg_usage.value,
+            mem_lim: cur_metrics.mem_limit.value,
+            swp_use: cur_metrics.mem_swap.value,
+            swp_lim: cur_metrics.mem_swap_limit.value,
+            zfs_use: cur_metrics.zfs_used.value,
+            zfs_av: cur_metrics.zfs_available.value,
+            netb_in: cur_metrics.net_agg_bytes_in.value,
+            netb_in_old: last_metrics.net_agg_bytes_in ?
+                last_metrics.net_agg_bytes_in.value : 0,
+            netb_out: cur_metrics.net_agg_bytes_out.value,
+            netb_out_old: last_metrics.net_agg_bytes_out ?
+                last_metrics.net_agg_bytes_out.value : 0,
+            cpu_usr: cur_metrics.cpu_user_usage.value,
+            cpu_usr_old: last_metrics.cpu_user_usage ?
+                last_metrics.cpu_user_usage.value : 0,
+            cpu_sys: cur_metrics.cpu_sys_usage.value,
+            cpu_sys_old: last_metrics.cpu_sys_usage ?
+                last_metrics.cpu_sys_usage.value : 0,
+            load_avg: cur_metrics.load_average.value
         };
 
         zone_stats.push(stats);
@@ -176,6 +196,7 @@ function draw() {
         zfs_avail += (((za_inst / 1000) / 1000) / 1000);
 
 
+        /* Total CPU % */
         var cpu_usr_ttl = parseInt(stats.cpu_usr);
         var cpu_usr_old_ttl = parseInt(stats.cpu_usr_old);
         var cpu_usr_inc_ttl = (cpu_usr_ttl - cpu_usr_old_ttl);
@@ -190,10 +211,17 @@ function draw() {
         var net_in_diff = netb_in - netb_in_old;
         var net_in_mb = ((net_in_diff / 1000) / 1000);
         net_in_mb_per_sec += (net_in_mb / 10);
+
+        /* Egress Network Bytes */
+        var netb_out = parseInt(stats.netb_out);
+        var netb_out_old = parseInt(stats.netb_out_old);
+        var net_out_diff = netb_out - netb_out_old;
+        var net_out_mb = ((net_out_diff / 1000) / 1000);
+        net_out_mb_per_sec += (net_out_mb / 10);
     }
 
     var GB = ' GB';
-    var blank_line = new line().fill().output();
+    var blank_line = new line().fill();
 
     /* Memory Totals Output */
     var mem_danger = mem_limit * 0.8;
@@ -208,7 +236,7 @@ function draw() {
 
     var mem_line = new line();
     mem_line.padding(2);
-    mem_line.column('Total Memory Use', 20, [mod_clc.cyan]);
+    mem_line.column('DRAM', 20, [mod_clc.cyan]);
     mem_line.column(mem_gauge);
     mem_line.fill();
     mem_line.output();
@@ -226,7 +254,7 @@ function draw() {
 
     var swap_line = new line();
     swap_line.padding(2);
-    swap_line.column('Total Swap Use', 20, [mod_clc.cyan]);
+    swap_line.column('Swap', 20, [mod_clc.cyan]);
     swap_line.column(swap_gauge);
     swap_line.fill();
     swap_line.output();
@@ -245,14 +273,14 @@ function draw() {
 
     var zfs_line = new line();
     zfs_line.padding(2);
-    zfs_line.column('Total ZFS Use', 20, [mod_clc.cyan]);
+    zfs_line.column('ZFS', 20, [mod_clc.cyan]);
     zfs_line.column(zfs_gauge);
     zfs_line.fill();
     zfs_line.output();
 
     /* CPU % Totals Output */
     var cpu_gauge = gauge(
-        cpu_pct,
+        cpu_pct.toFixed(0),
         400,
         20,
         400 * 0.8,
@@ -260,32 +288,44 @@ function draw() {
 
     var cpu_line = new line();
     cpu_line.padding(2);
-    cpu_line.column('Total CPU %', 20, [mod_clc.cyan]);
+    cpu_line.column('CPU', 20, [mod_clc.cyan]);
     cpu_line.column(cpu_gauge);
     cpu_line.fill();
     cpu_line.output();
 
     blank_line.output();
 
+    /* Ingress MBps */
     ingressNet.push(net_in_mb_per_sec.toFixed(2));
     ingressNet.shift();
     var netb_in_line = new line();
     netb_in_line.padding(2);
-    netb_in_line.column('MBps', 20, [mod_clc.cyan]);
+    netb_in_line.column('Ingress MBps', 20, [mod_clc.cyan]);
     netb_in_line.column(spark(ingressNet, ' MBps'), 80);
     netb_in_line.fill();
     netb_in_line.output();
 
+    /* Egress MBps */
+    egressNet.push(net_out_mb_per_sec.toFixed(2));
+    egressNet.shift();
+    var netb_out_line = new line();
+    netb_out_line.padding(2);
+    netb_out_line.column('Egress MBps', 20, [mod_clc.cyan]);
+    netb_out_line.column(spark(egressNet, ' MBps'), 80);
+    netb_out_line.fill();
+    netb_out_line.output();
+
     blank_line.output();
 
+    /* Individual Zone Column Headers */
     var zone_col_names = new line();
     zone_col_names.padding(2);
-    zone_col_names.column('Zone Name', 15, [mod_clc.cyan]);
-    zone_col_names.column('Memory in MB', 15, [mod_clc.cyan]);
-    zone_col_names.column('Swap in MB', 15, [mod_clc.cyan]);
-    zone_col_names.column('ZFS in GB', 15, [mod_clc.cyan]);
-    zone_col_names.column('Load Avg', 15, [mod_clc.cyan]);
-    zone_col_names.column('CPU %', 15, [mod_clc.cyan]);
+    zone_col_names.column('Container', 15, [mod_clc.cyan]);
+    zone_col_names.column('DRAM', 10, [mod_clc.cyan]);
+    zone_col_names.column('Swap', 15, [mod_clc.cyan]);
+    zone_col_names.column('ZFS', 10, [mod_clc.cyan]);
+    zone_col_names.column('Load Avg', 10, [mod_clc.cyan]);
+    zone_col_names.column('CPU %', 10, [mod_clc.cyan]);
     zone_col_names.fill();
     zone_col_names.output();
 
@@ -297,13 +337,14 @@ function draw() {
 
         /* Mem usage */
         var mem_use = stats.mem_use;
-        var mem_use_human = (((mem_use / 1000) / 1000));
-        var mem_use_human = mem_use_human.toFixed(0);
+        var mem_use_human = (((mem_use / 1000) / 1000) / 1000);
+        var mem_use_human = mem_use_human.toFixed(1);
         var mem_lim = stats.mem_lim;
-        var mem_lim_human = (((mem_lim / 1000) / 1000));
-        var mem_lim_human = mem_lim_human.toFixed(0);
-        var mem_col = mem_use_human + ' / ' + mem_lim_human;
-        zone_line.column(mem_col, 15, [mod_clc.white]);
+        var mem_lim_human = (((mem_lim / 1000) / 1000) / 1000);
+        var mem_lim_human = mem_lim_human.toFixed(1);
+        var mem_use_pct = ((mem_use * 100) / mem_lim).toFixed(1);
+        var mem_col = mem_use_pct + '%';
+        zone_line.column(mem_col, 10, [mod_clc.white]);
 
         /* Swap usage */
         var swp_use = stats.swp_use;
@@ -312,7 +353,7 @@ function draw() {
         var swp_lim = stats.swp_lim;
         var swp_lim_human = (((swp_lim / 1000) / 1000));
         var swp_lim_human = swp_lim_human.toFixed(0);
-        var swp_col = swp_use_human + ' / ' + swp_lim_human;
+        var swp_col = swp_use_human + '/' + swp_lim_human + ' MB';
         zone_line.column(swp_col, 15, [mod_clc.white]);
 
         /* ZFS usage */
@@ -322,21 +363,22 @@ function draw() {
         var zfs_lim = parseInt(stats.zfs_av) + parseInt(zfs_use);
         var zfs_lim_human = ((zfs_lim / 1000) / 1000) / 1000;
         var zfs_lim_human = zfs_lim_human.toFixed(0);
-        var zfs_col = zfs_use_human + ' / ' + zfs_lim_human;
-        zone_line.column(zfs_col, 15, [mod_clc.white]);
+        var zfs_col = zfs_use_human + '/' + zfs_lim_human + ' GB';
+        zone_line.column(zfs_col, 10, [mod_clc.white]);
 
         /* Load Average */
         var load_avg_col = parseFloat(stats.load_avg);
-        zone_line.column(load_avg_col.toFixed(2), 15, [mod_clc.white]);
+        zone_line.column(load_avg_col.toFixed(2), 10, [mod_clc.white]);
 
+        /* CPU % */
         var cpu_usr = parseInt(stats.cpu_usr);
         var cpu_usr_old = parseInt(stats.cpu_usr_old);
         var cpu_usr_inc = (cpu_usr - cpu_usr_old);
         var cpu_sys = parseInt(stats.cpu_sys);
         var cpu_sys_old = parseInt(stats.cpu_sys_old);
         var cpu_sys_inc = (cpu_sys - cpu_sys_old);
-        var cpu_pct_col = ((cpu_sys_inc + cpu_usr_inc) / 10000000000) * 100;
-        zone_line.column(cpu_pct_col.toFixed(2), 15, [mod_clc.white]);
+        var cpu_pct_col = ((cpu_sys_inc + cpu_usr_inc) / NANO_SEC_PERIOD) * 100;
+        zone_line.column(cpu_pct_col.toFixed(2), 10, [mod_clc.white]);
 
         /* draw the zone line */
         zone_line.fill();
